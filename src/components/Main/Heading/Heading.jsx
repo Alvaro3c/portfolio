5bc7abab9feb0cf05856e50a59016fb85f931174
unknown <alvaro.cuallado.g@gmail.com>
2024-02-26 13:40:26 +0100

    add/heading has techonoligcal skills with labels

--- src/components/Main/Heading/Heading.jsx
+++ src/components/Main/Heading/Heading.jsx
@@ -7,20 +7,38 @@ const Heading = () => {
       <article>
         <h1>Hi there, Im Alvaro Cuallado</h1>
         <h2>Full Stack developer</h2>
-
-        {/* iconos de tecnologias conocidas */}
         <div className="icons-box">
-          <div className="icons-group-1">
+          <div className="icons-with-label">
             <img src="./5968242.png" alt="" />
-            <img src="./1200px-Javascript-shield.svg.png" alt="" />
-            <img src="./Angular_full_color_logo.svg.png" alt="" />
-            <img src="./1280px-Sass_Logo_Color.svg.png" alt="" />
+            <p>CSS</p>
           </div>
-          <div className="icons-group-2">
+          <div className="icons-with-label">
+            <img src="./1200px-Javascript-shield.svg.png" alt="" />
+            <p>Javascript</p>
+          </div>
+          <div className="icons-with-label">
+            <img src="./Angular_full_color_logo.svg.png" alt="" />
+            <p>Angular</p>
+          </div>
+          <div className="icons-with-label">
+            <img src="./1280px-Sass_Logo_Color.svg.png" alt="" />
+            <p>SASS</p>
+          </div>
+          <div className="icons-with-label">
             <img src="./nodejs_original_wordmark_logo_icon_146412.png" alt="" />
+            <p>NodeJS</p>
+          </div>
+          <div className="icons-with-label">
             <img src="./MongoDB_Logo.svg.png" alt="" />
+            <p>MongoDB</p>
+          </div>
+          <div className="icons-with-label">
             <img src="./4299956.png" alt="" />
+            <p>SQL</p>
+          </div>
+          <div className="icons-with-label">
             <img src="./Git-Icon-1788C.png" alt="" />
+            <p>git</p>
           </div>
         </div>
       </article>
